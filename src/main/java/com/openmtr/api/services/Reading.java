3449a03b6ad436b4b6ab9afd7427e7d45830dfed
Matthew Thomas <mdthomas_2009@hotmail.com>
2019-02-18 20:21:35 -0500

    feat(API) add connection information and accessors to the class

--- src/main/java/com/openmtr/api/services/Reading.java
+++ src/main/java/com/openmtr/api/services/Reading.java
@@ -1,7 +1,26 @@
 package com.openmtr.api.services;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
 public class Reading {
 
+		public static final String DATAURL = "jdbc:sqlserver://openmeter-data.database.windows.net:1433;database=Openmtr;user=openmtr-admin@openmeter-data;password=OMeter@@99;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
+		//public static final String URL = "jdbc:sqlserver://openmeter-data.database.windows.net:1433";
+		//public static final STring database = "Openmtr";
+		//public static final String USER = "openmtr-admin@openmeter-data" ;
+		//public static final String PASSWORD = "OMeter@@99";
+		//encrypt=true;
+		//trustServerCertificate=false;
+		//hostNameInCertificate=*.database.windows.net;
+		//loginTimeout=30;
+	
+	
 		/* database layout:
 		 * int (auto increment)
 		 * nchar (255) url NOT NULL
@@ -18,6 +37,8 @@ public class Reading {
 		 * updatedBy nchar(30) NULL
 		 * updatedDate Date NULL
 		 */
+	
+	
 			private String url;
 			private String date;
 			private String buildVersion;
@@ -104,6 +125,115 @@ public class Reading {
 				this.updatedDate = null;
 			}
 			
+			public String getUrl() {
+				return url;
+			}
+			
+			public void setUrl(String url) {
+				this.url = url;
+			}
+			
+			public String getDate() {
+				return date;
+			}
+			
+			public void setDate(String date) {
+				this.date = date;
+			}
+			
+			public String getBuildVersion() {
+				return buildVersion;
+			}
+			
+			public void setBuildVersion(String buildVersion) {
+				this.buildVersion = buildVersion;
+			}
+			
+			public String getRead() {
+				return read;
+			}
+			
+			public void setRead(String read) {
+				this.read = read;
+			}
+			
+			public String getReadMethod() {
+				return readMethod;
+			}
+			
+			public void setReadMethod(String readMethod) {
+				this.readMethod = readMethod;
+			}
+			
+			public String getTotalProcessingTime() {
+				return totalProcessingTime;
+			}
+			
+			public void setTotalProcessingTime(String totalProcessingTime) {
+				this.totalProcessingTime = totalProcessingTime;
+			}
+			
+			public boolean getsuccess() {
+				return success;
+			}
+			
+			public void setSuccess(boolean success) {
+				this.success = success;
+			}
+			
+			public String getCreatedBy() {
+				return createdBy;
+			}
+			
+			public void setCreatedBy(String createdBy) {
+				this.createdBy = createdBy;
+			}
+			
+			public String getCreatedDate() {
+				return this.createdDate;
+			}
+			
+			public void setCreatedDate(String createdDate) {
+				this.createdDate = createdDate;
+			}
+			
+			public String getMeterType() {
+				return this.meterType;
+			}
+			
+			public void setMeterType(String meterType) {
+				this.meterType = meterType;
+			}
+			
+			public String getErrorCode() {
+				return this.errorCode;
+			}
+			
+			public void setErrorCode(String errorCode) {
+				this.errorCode = errorCode;
+			}
+			
+			public String getUpdatedBy() {
+				return this.updatedBy;
+			}
+			
+			public void setUpdatedBy(String updatedBy) {
+				this.updatedBy = updatedBy;
+			}
+			
+			
+			// connect to the azure sql databasef
+			// if connection doesn't work, thrown an error
+			public static Connection getConnection() {
+				try {
+					
+					return DriverManager.getConnection(DATAURL);
+				}
+				catch (SQLException ex) {
+					throw new RuntimeException("Something went wrong connecting to the database.", ex);
+				}
+			}
+}
 		
 				
 			
