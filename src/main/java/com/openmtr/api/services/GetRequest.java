b25e23d2309718f38a8f4eb297f76b7579fd85be
James Willhoite <jameswillhoite@gmail.com>
2019-03-04 20:29:01 -0500

    feat(API): Created GetRequest Class

---
+++ src/main/java/com/openmtr/api/services/GetRequest.java
@@ -0,0 +1,24 @@
+package com.openmtr.api.services;
+
+public class GetRequest extends ApiRequest {
+
+	@Override
+	public void setEmailAddress(String email) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDialsOnMeter(String dialsOnMeter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean savedImage() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
