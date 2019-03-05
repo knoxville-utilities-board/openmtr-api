cd9f67825cd32f8945bd1e6b1f2394aac1584bc7
Matthew Thomas <mdthomas_2009@hotmail.com>
2019-03-05 14:25:40 -0500

    allow connection to azure with connection string, and start insert class

--- src/main/java/com/openmtr/api/services/Reading.java
+++ src/main/java/com/openmtr/api/services/Reading.java
@@ -10,19 +10,11 @@ import java.sql.ResultSet;
 
 public class Reading {
 
-	public static final String DATAURL = "jdbc:sqlserver://openmeter-data.database.windows.net:1433;database=Openmtr;user=openmtr-admin@openmeter-data;password=OMeter@@99;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
-	// public static final String URL =
-	// "jdbc:sqlserver://openmeter-data.database.windows.net:1433";
-	// public static final STring database = "Openmtr";
-	// public static final String USER = "openmtr-admin@openmeter-data" ;
-	// public static final String PASSWORD = "OMeter@@99";
-	// encrypt=true;
-	// trustServerCertificate=false;
-	// hostNameInCertificate=*.database.windows.net;
-	// loginTimeout=30;
+	public static final String DATAURL = System.getenv("JDBC-SQL-Azure-OpenMTR");
+	
 
 	/*
-	 * database layout: int (auto increment) nchar (255) url NOT NULL date
+	 * database layout: int Id(auto increment), nchar (255) url NOT NULL,
 	 * yyyy-mm-dd uploadDate NOT NULL nchar (10) buildVersion NOT NULL nchar (10)
 	 * read NOT NULL nchar (25) readMethod NOT NULL nchar (25) meterType
 	 * totalProcessingTime nchar (15) NOT NULL nchar (50) errorCode bit success NOT
@@ -31,7 +23,7 @@ public class Reading {
 	 */
 
 	private String url;
-	private String date;
+	private String uploadDate;
 	private String buildVersion;
 	private String read;
 	private String readMethod;
@@ -53,15 +45,15 @@ public class Reading {
 	/*
 	 * constructor with all values required
 	 * 
-	 * @params url, date, buildVersion, read, readMethod totalProcessingTime,
+	 * @params url, uploadDate, buildVersion, read, readMethod totalProcessingTime,
 	 * success boolean, createdBy, createdDate (yyyy-mm-dd) renders meterType,
 	 * errorCode, updatedBy, and updatedDate null this will be the most commonly
 	 * used constructor
 	 */
-	public Reading(String url, String date, String buildVersion, String read, String readMethod,
+	public Reading(String url, String uploadDate, String buildVersion, String read, String readMethod,
 			String totalProcessingTime, boolean success, String createdBy, String createdDate) {
 		this.url = url;
-		this.date = date;
+		this.uploadDate = uploadDate;
 		this.buildVersion = buildVersion;
 		this.read = read;
 		this.readMethod = readMethod;
@@ -82,10 +74,10 @@ public class Reading {
 	 * success boolean, createdBy, createdDate (yyyy-mm-dd) renders errorCode,
 	 * updatedBy, and updatedDate null
 	 */
-	public Reading(String url, String date, String buildVersion, String read, String readMethod,
+	public Reading(String url, String uploadDate, String buildVersion, String read, String readMethod,
 			String totalProcessingTime, boolean success, String createdBy, String createdDate, String meterType) {
 		this.url = url;
-		this.date = date;
+		this.uploadDate = uploadDate;
 		this.buildVersion = buildVersion;
 		this.read = read;
 		this.readMethod = readMethod;
@@ -106,11 +98,11 @@ public class Reading {
 	 * success boolean, createdBy, createdDate (yyyy-mm-dd), meterType, errorCode
 	 * renders updatedBy, and updatedDate null
 	 */
-	public Reading(String url, String date, String buildVersion, String read, String readMethod,
+	public Reading(String url, String uploadDate, String buildVersion, String read, String readMethod,
 			String totalProcessingTime, boolean success, String createdBy, String createdDate, String meterType,
 			String errorCode) {
 		this.url = url;
-		this.date = date;
+		this.uploadDate = uploadDate;
 		this.buildVersion = buildVersion;
 		this.read = read;
 		this.readMethod = readMethod;
@@ -123,6 +115,22 @@ public class Reading {
 		this.updatedBy = null;
 		this.updatedDate = null;
 	}
+	
+	// default update constructor
+	public Reading(String updatedBy, String updatedDate) {
+		this.updatedBy = updatedBy;
+		this.updatedDate = updatedDate;
+		this.url = null;
+		this.createdDate = null;
+		this.meterType = null;
+		this.errorCode = null;
+		this.buildVersion = null;
+		this.read = null;
+		this.readMethod = null;
+		this.totalProcessingTime = null;
+		this.createdBy = null;
+	}
+		
 
 	public String getUrl() {
 		return url;
@@ -207,7 +215,7 @@ public class Reading {
 	public String getErrorCode() {
 		return this.errorCode;
 	}
-
+	
 	public void setErrorCode(String errorCode) {
 		this.errorCode = errorCode;
 	}
@@ -219,15 +227,140 @@ public class Reading {
 	public void setUpdatedBy(String updatedBy) {
 		this.updatedBy = updatedBy;
 	}
+	
+	
 
-	// connect to the azure sql databasef
+	// connect to the azure sql database
 	// if connection doesn't work, thrown an error
 	public static Connection getConnection() {
 		try {
-
-			return DriverManager.getConnection(DATAURL);
+			if (DATAURL != null) {
+				return DriverManager.getConnection(DATAURL);
+			}
+			else throw new RuntimeException("No connection string was provided.");
 		} catch (SQLException ex) {
 			throw new RuntimeException("Something went wrong connecting to the database.", ex);
 		}
 	}
+	
+	public boolean insertReading(Connection azureData) {
+		String columnValues = "(";
+		String dataValues = "(";
+		if (this.updatedBy != null) {
+			columnValues = columnValues + " updatedBy,";
+			dataValues = dataValues + " " + this.updatedBy + ",";
+		}
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		if (this.updatedDate != null) {
+			columnValues = columnValues + " updatedDate,";
+			dataValues = dataValues + " " + this.updatedDate + ",";
+		}
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		if (url != null) {
+			columnValues = columnValues + " url,";
+			dataValues = dataValues + " " + this.url + ",";
+		}
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		if (meterType != null) {
+			columnValues = columnValues + " meterType,";
+			dataValues = dataValues + " " + this.meterType + ",";
+		}
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		if (this.errorCode != null) {
+			columnValues = columnValues + " errorCode,";
+			dataValues = dataValues + " " + this.errorCode + ",";
+		}
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		if (this.buildVersion != null) {
+			columnValues = columnValues + " buildVersion,";
+			dataValues = dataValues + " " + this.buildVersion + ",";
+		}
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		
+		if (read != null) {
+			columnValues = columnValues + " read,";
+			dataValues = dataValues + " " + this.read + ",";
+		}
+		
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		
+		if (this.readMethod != null) {
+			columnValues = columnValues + " readMethod,";
+			dataValues = dataValues + " " + this.readMethod + ",";
+		}
+		
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		
+		if (this.totalProcessingTime != null) {
+			columnValues = columnValues + " totalProcessingTime,";
+			dataValues = dataValues + " " + this.totalProcessingTime + ",";
+		}
+		
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		
+		if (this.createdBy != null) {
+			columnValues = columnValues + " createdBy,";
+			dataValues = dataValues + " " + this.createdBy + ",";
+		}
+		
+		else {
+			columnValues = columnValues + "";
+			dataValues = dataValues + "";
+		}
+		
+		// take the last comment off the two strings so we get a good insert
+		columnValues = peskyComma(columnValues);
+		dataValues = peskyComma(columnValues);
+		
+		// add the strings together in the insertion string
+		
+		//String insertion = "INSERT INTO reading(
+		
+		try {
+		
+		}
+	}
+	
+	// gets rid of that pesky comma on the end of an aggregate value or data string.
+	public String peskyComma(String commaString) {
+		if (commaString.charAt(commaString.length() - 1) == ',') {
+			commaString = commaString.substring(0, commaString.length() - 1);
+			return commaString;
+		}
+		else {
+			return commaString;
+		}
+	}
+	
+	
+	
+	
+	// method to create insert statement
 }
