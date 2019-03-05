3206e5fcc332c567397689b54eeede78b3206836
James Willhoite <jameswillhoite@gmail.com>
2019-03-04 21:43:13 -0500

    feat(API): Added GetRequest

--- src/main/java/com/openmtr/api/services/GetRequest.java
+++ src/main/java/com/openmtr/api/services/GetRequest.java
@@ -8,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.ws.rs.QueryParam;
 
@@ -34,12 +36,29 @@ public class GetRequest extends ApiRequest {
 	protected boolean savedImage() {
 		try {
 			this.downloadImage(this.url);
+			this.extractByteArray();
 			return true;
 		} catch (FileNotFoundException ex) {
 			this.setErrorMsg(ex.getMessage());
 			return false;
 		}
 		
+	}
+	
+	public boolean validateRequest() {
+		if(!this.isValidEmail()) {
+			this.setErrorMsg("Email address is invalid");
+		}
+		else if (!this.isValidDialsOnMeter()) {
+			this.setErrorMsg("The dials on meter is invalid");
+		}
+		else if (!this.isValidUrl()) {
+			this.setErrorMsg("The URL address is invalid");
+		}
+		else if(!this.savedImage()) {
+			this.setErrorMsg("Could not download the image.");
+		}
+		return this.error;
 	}
 	
 	
@@ -61,6 +80,11 @@ public class GetRequest extends ApiRequest {
 		
 	}
 
+	private boolean isValidUrl() {
+		Pattern reg = Pattern.compile("^((http[s]?|ftp):\\/)?\\/?([^:\\/\\s]+)((\\/\\w+)*\\/)([\\w\\-\\.]+[^#?\\s]+)(.*)?(#[\\w\\-]+)?$");
+		Matcher m = reg.matcher(this.url);
+		return m.find();
+	}
 	
 
 }
--- src/main/java/com/openmtr/api/services/OpenMeterApi.java
+++ src/main/java/com/openmtr/api/services/OpenMeterApi.java
@@ -1,5 +1,7 @@
 package com.openmtr.api.services;
 
+import java.io.IOException;
+
 //import java.awt.Image;
 //import java.awt.image.BufferedImage;
 //import java.awt.image.DataBufferByte;
@@ -22,6 +24,7 @@ package com.openmtr.api.services;
 //import javax.imageio.stream.ImageInputStream;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 //import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -35,73 +38,28 @@ import com.mattclinard.openmtr.OpenMeter;
 public class OpenMeterApi {
 	
 	private ReturnResponse rr = new ReturnResponse();
-	
-	
-//	public OpenMeterApi() {
-//		
-//	}
-	
-	
 
-//	@GET
-//	@Produces("application/json")
-//	public Response downloadFromUrl(
-//			@QueryParam("url") String url, 
-//			@QueryParam("numberOfDigits") String digits
-//			) {
-//		//Init the Return Response Class
-//		ReturnResponse rr = new ReturnResponse();
-//		
-//		try {
-//			String testVar = System.getenv("TEST");
-//		//Check to see if a URL was provided
-//		if(url.isEmpty())
-//			return rr.error("URL is missing (Environment Variable = " + testVar + ")", 400);
-//		} catch(NullPointerException ex) {
-//			return rr.error("URL is missing", 400);
-//		}
-//		
-//		//Make sure the URL is a valid URL
-//		if(!this.validateURL(url))
-//        	return rr.error("The given url is invalid. Please provide a format of http(s)://domain.com/image.extension", 400);
-//		
-//		if(!this.validateDigitsOnMeterFace(digits))	
-//			return rr.error("Error", 400);
-//		
-//		//Download the image from the URL
-//		String imagePath = null;
-//		try {
-//			imagePath = this.downloadImage(url);
-//		} catch (Exception ex) {
-//			return rr.error(ex.getMessage(), this.statusCode);
-//		}
-//		
-//		//Extract the byte[] from the downloaded image
-//		byte[] image = null;
-//		try {
-//			image = this.extractByteArray(imagePath);
-//		} catch (Exception ex) {
-//			return rr.error(ex.getMessage(), this.statusCode);
-//		}
-//		
-//		//Test the byte[] against Matt C's library
-//		String meterRead = "";
-//		OpenMeter om = new OpenMeter();
-//		try {
-//			meterRead = om.getMeterRead(image, "9999");
-//		} catch (IOException ex) {
-//			return rr.error("Could not Read Meter. ", 400);
-//		} catch (NullPointerException ex) {
-//			return rr.error("Problem with AI, fix comming", 500);
-//		}
-//		
-//		
-//		//All good, return the ok
-//		rr.setData(meterRead);
-//		return rr.success();
-//
-//		
-//	}
+
+	@GET
+	@Produces("application/json")
+	public Response downloadFromUrl(@BeanParam GetRequest getRequest) {
+		if(!getRequest.validateRequest()) {
+			rr.error(getRequest.getErrorMsg(), 400);
+		}
+		
+		String meterRead = "";
+		OpenMeter om = new OpenMeter();
+		try {
+			meterRead = om.getMeterRead(getRequest.getImageByteArray(), getRequest.getDialsOnMeter());
+		} catch (IOException ex) {
+			return rr.error("Could not Read Meter. ", 400);
+		}
+		
+		rr.setData(meterRead);
+		return rr.success();
+
+		
+	}
 	
 	@POST
 	@Produces("application/json")
@@ -122,58 +80,5 @@ public class OpenMeterApi {
 		rr.setData(meterRead);
 		return rr.success();
 	}
-
-	
-	/**
-	 * Download the image from the given URL
-	 * @param String url
-	 * @return String Image File Location
-	 * @throws Exception
-	 */
-//	private String downloadImage(String url) throws Exception{
-//		this.saveImageFolder = this.servletContext.getRealPath("/") + "uploadedImages/";
-//		//Need to make sure the Images folder exists
-//		File dir = new File(this.saveImageFolder);
-//		if(!dir.exists()) {
-//			this.statusCode = 500;
-//			throw new Exception("Image Folder doesn't exist");
-//		}
-//		
-//		//Get the file name from the url
-//		String fileName = url.split("/")[url.split("/").length-1];
-//		
-//		
-//		//Do the actual downloading of the image
-//		try {
-//			InputStream in = new URL(url).openStream();
-//			Files.copy(in, Paths.get(this.saveImageFolder + fileName), StandardCopyOption.REPLACE_EXISTING);
-//		} catch(Exception ex) {
-//			this.statusCode = 400;
-//			throw new Exception("Could not download image from URL: " + url);
-//		}
-//		
-//		//Make sure the file was downloaded
-//		File imageFile = new File(this.saveImageFolder + fileName);
-//		if(!imageFile.exists()) {
-//			this.statusCode = 404;
-//			throw new Exception("Image was not downloaded.");
-//		}
-//		
-//		//Make sure the downloaded file is a image and has the correct file type extension
-//		String imagePath;
-//		try {
-//			imagePath = this.determineFileType(this.saveImageFolder + fileName);
-//		} catch (Exception ex) {
-//			this.statusCode = 400;
-//			throw new Exception(ex.getMessage());
-//		}
-//		
-//		
-//		
-//		
-//		//return the image location
-//		return imagePath;
-//		
-//	}
 	
 }
