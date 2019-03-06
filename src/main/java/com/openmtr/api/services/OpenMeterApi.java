440d439a3963af6515af12af76303d0e892fb5ea
James Willhoite <jameswillhoite@gmail.com>
2019-03-06 08:39:27 -0500

    fix(API): Removed unused imports

--- src/main/java/com/openmtr/api/services/OpenMeterApi.java
+++ src/main/java/com/openmtr/api/services/OpenMeterApi.java
@@ -2,34 +2,12 @@ package com.openmtr.api.services;
 
 import java.io.IOException;
 
-//import java.awt.Image;
-//import java.awt.image.BufferedImage;
-//import java.awt.image.DataBufferByte;
-//import java.awt.image.WritableRaster;
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.OutputStream;
-//import java.net.URL;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.nio.file.StandardCopyOption;
-//import java.util.Iterator;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
-//
-//import javax.imageio.ImageIO;
-//import javax.imageio.ImageReader;
-//import javax.imageio.stream.ImageInputStream;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-//import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-//import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import com.mattclinard.openmtr.OpenMeter;
